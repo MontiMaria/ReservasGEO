d601715f15855a811ee922dcf1eb107cd6d7b500
Ramiro Martinez <ramiro.nicolas.martinez14@gmail.com>
2025-09-09 09:35:29 -0300

    Correccion del servicio ver_list_reservas_activas

--- Services/RecursosService.php
+++ Services/RecursosService.php
@@ -77,29 +77,14 @@ class RecursosService
     public function ver_listado_reservas_activas($id, Request $request)
     {
         $data = $request->all();
-        try {
-            $informe = $this->RecursosService->ver_listado_reservas_activas($id, $data['id_usuario'], $data['id_nivel'], $data['cant_por_pagina'] ?? null, $data['pagina'] ?? null);
+         try {
+            return $this->RecursosRep->ver_listado_reservas_activas($id, $id_usuario, $id_nivel, $cant_por_pagina, $pagina);
+        } catch (InvalidArgumentException $e) {
+            throw new InvalidArgumentException("El rol no tiene permisos para ver las reservas activas.",403);
 
-            return response()->json([
-                'success' => true,
-                'data' => $informe,
-                'messages' => '',
-            ]);
-        }catch (InvalidArgumentException $e) {
-            // Caso específico: rol sin permisos
-            return response()->json([
-                'success' => false,
-                'data' => null,
-                'messages' => $e->getMessage(),
-            ], 403);
-        }catch (Exception $e) {
-            Log::error("CONTROLLER ERROR: " . $e->getMessage(), ['exception' => $e]);
-
-            return response()->json([
-                'success' => false,
-                'data' => null,
-                'messages' => 'Error en la obtencion de reservas activas' . $e->getMessage(),
-            ], 500);
+        } catch (Exception $e) {
+            Log::error("ERROR: " . $e->getMessage() . " - linea " . $e->getLine(), ['exception' => $e]);
+            return $e;
         }
     }
 
